6b13eff739c08f6726de04e93bb894dfb5118010
Colm Britton <colmjude@gmail.com>
2020-11-20 16:56:25 +0000

    Render a guidance page with a contents section

---
+++ bin/markdown.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import markdown
+from markdown.extensions.toc import TocExtension
+
+# basic markdown setup
+md = markdown.Markdown(extensions=[TocExtension(toc_depth="2")])
+
+
+def apply_our_classes(html):
+    html = html.replace("<p>", '<p class="govuk-body">')
+    html = html.replace("<h1>", '<h1 class="govuk-heading-xl">')
+    html = html.replace("<h2>", '<h2 class="govuk-heading-l">')
+    html = html.replace("<h3>", '<h3 class="govuk-heading-m">')
+    html = html.replace("<h4>", '<h4 class="govuk-heading-s">')
+    html = html.replace("<ul>", '<ul class="govuk-list govuk-list--bullet">')
+    html = html.replace("<pre>", '<pre class="hljs-container">')
+    return html
+
+
+def markdown_compile(s):
+    html = md.convert(s)
+    html = apply_our_classes(html)
+    return html
+
+
+def get_contents_section():
+    contents = md.toc
+    contents = contents.replace(
+        '<div class="toc">',
+        '<div class="govuk-grid-column-one-third contents-section">\n<p>Contents</p>',
+    )
+    return contents
--- render.py
+++ render.py
@@ -5,11 +5,19 @@ import sys
 import json
 
 from bin.jinja_setup import setup_jinja, render
+from bin.markdown import markdown_compile, get_contents_section
+
+from frontmatter import Frontmatter
 
 env = setup_jinja()
 env.globals["staticPath"] = "https://digital-land.github.io"
 # get templates
 index_template = env.get_template("index.html")
+guidance_template = env.get_template("guidance.html")
+
+
+def markdown_files_only(files, file_ext=".md"):
+    return [f for f in files if f.endswith(file_ext)]
 
 
 def generate_guidance_pages():
@@ -17,6 +25,24 @@ def generate_guidance_pages():
         data = json.load(file)
 
     render("index.html", index_template, pages=data["pages"])
+
+    guidance_dir = "content/"
+    sections = os.listdir(guidance_dir)
+
+    print(sections)
+    for section in sections:
+        page_content = Frontmatter.read_file(f"{guidance_dir}{section}/index.md")
+        html = markdown_compile(page_content["body"])
+
+        content = {"main": html}
+        if page_content["attributes"]["hasContents"]:
+            content["contents"] = get_contents_section()
+        render(
+            f"{section}/index.html",
+            guidance_template,
+            content=content,
+            fm=page_content["attributes"],
+        )
 
 
 if __name__ == "__main__":
