9dfd4e9184a7dd7a1b9585098215a8df3db405ec
Colm Britton <colmjude@gmail.com>
2020-11-22 18:23:47 +0000

    Add more classes to markdown output

--- bin/markdown.py
+++ bin/markdown.py
@@ -9,11 +9,12 @@ md = markdown.Markdown(extensions=[TocExtension(toc_depth="2")])
 
 def apply_our_classes(html):
     html = html.replace("<p>", '<p class="govuk-body">')
-    html = html.replace("<h1>", '<h1 class="govuk-heading-xl">')
-    html = html.replace("<h2>", '<h2 class="govuk-heading-l">')
-    html = html.replace("<h3>", '<h3 class="govuk-heading-m">')
-    html = html.replace("<h4>", '<h4 class="govuk-heading-s">')
+    html = html.replace("<h1", '<h1 class="govuk-heading-xl" ')
+    html = html.replace("<h2", '<h2 class="govuk-heading-l" ')
+    html = html.replace("<h3", '<h3 class="govuk-heading-m" ')
+    html = html.replace("<h4 ", '<h4 class="govuk-heading-s" ')
     html = html.replace("<ul>", '<ul class="govuk-list govuk-list--bullet">')
+    html = html.replace("<ol>", '<ol class="govuk-list govuk-list--number">')
     html = html.replace("<pre>", '<pre class="hljs-container">')
     return html
 
