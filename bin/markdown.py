712d198b68c7bce8d48cd7651195f747a2f708dc
Colm Britton <colmjude@gmail.com>
2020-11-23 11:04:19 +0000

    Add more dl classes to markdown output

--- bin/markdown.py
+++ bin/markdown.py
@@ -12,10 +12,11 @@ def apply_our_classes(html):
     html = html.replace("<h1", '<h1 class="govuk-heading-xl" ')
     html = html.replace("<h2", '<h2 class="govuk-heading-l" ')
     html = html.replace("<h3", '<h3 class="govuk-heading-m" ')
-    html = html.replace("<h4 ", '<h4 class="govuk-heading-s" ')
+    html = html.replace("<h4", '<h4 class="govuk-heading-s" ')
     html = html.replace("<ul>", '<ul class="govuk-list govuk-list--bullet">')
     html = html.replace("<ol>", '<ol class="govuk-list govuk-list--number">')
     html = html.replace("<pre>", '<pre class="hljs-container">')
+    html = html.replace("<code>", '<code class="dl-code">')
     return html
 
 
